a63461c2ab92ac20a1700bc7888ef6dfa711ecc7
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2022-01-18 16:43:04 -0300

    inserido navegação entre os menus

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -8,19 +8,19 @@ export function SidebarNav() {
     return (
         <Stack spacing="12" align="flex-start">
             <NavSection title="GERAL">
-                <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
+                <NavLink icon={RiDashboardLine} href="/dashboard">Dashboard</NavLink>
 
-                <NavLink icon={RiContactsLine}>
+                <NavLink icon={RiContactsLine} href="/users">
                     Usuários
                 </NavLink>
             </NavSection>
 
             <NavSection title="AUTOMAÇÃO">
-                <NavLink icon={RiInputMethodLine}>
+                <NavLink icon={RiInputMethodLine} href="/forms">
                     Formulários
                 </NavLink>
 
-                <NavLink icon={RiGitMergeLine}>
+                <NavLink icon={RiGitMergeLine} href="/automation">
                     Automação
                 </NavLink>
             </NavSection>
